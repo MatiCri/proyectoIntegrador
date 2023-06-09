1a98e9ee53892c2d4d1acbb9427f4d09ee3da1c6
gustavofmacia <gustavofmacia@gmail.com>
2023-06-08 21:40:40 -0300

    cambiada la anotación Component en cada implementación dao, por la anotación Repository

--- src/main/java/com/odontologia/ProyectoIntegrador/repository/impl/OdontologoDaoH2.java
+++ src/main/java/com/odontologia/ProyectoIntegrador/repository/impl/OdontologoDaoH2.java
@@ -6,12 +6,13 @@ import com.odontologia.ProyectoIntegrador.entity.Odontologo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Repository
 public class OdontologoDaoH2 implements IDao<Odontologo> {
 
     private static Logger LOGGER = LoggerFactory.getLogger(OdontologoDaoH2.class);
